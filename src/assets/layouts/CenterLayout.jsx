f47e82761b04f9752c47e0e06e210e70906ddbb6
AdrianZafari <adrian006@hotmail.com>
2025-05-28 23:27:56 +0200

    init

---
+++ src/assets/layouts/CenterLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+
+
+
+const CenterLayout = () => {
+    return (
+        <div className="center-wrapper">
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
+
+export default CenterLayout
